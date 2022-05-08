84e2aecb620b4f0fdfbe100618eb76353486dbb8
Afraj Attar <afraj.attar.ext@tribia.com>
2022-05-08 19:39:14 +0530

    Added base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-react-three.js-typescript-mui-template/",
   plugins: [react()]
 });
